27e35fa21ac18a66d905e8ae3e3c689c5a2f0393
Dalto <dalton.earl@outlook.com>
2020-04-25 18:06:53 -0600

    Added static to the server.js so it can actually serve the nd the css

--- server.js
+++ server.js
@@ -6,9 +6,11 @@ var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+app.use('/static', express.static("public"));
 
-require("./routes/apiRoutes")(app);
+// require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
+require("./routes/apiRoutes")(app);
 
 app.listen(PORT, function(){
     console.log(`App listening on PORT: ${PORT}`);
